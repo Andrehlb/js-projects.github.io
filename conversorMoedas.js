36fa2c16b4d9fccc5ec05821df4b8285761ce758
Andrehlb <andrehlb@me.com>
2023-11-15 16:27:47 -0300

    Create the index-conversor.html, and style-conversores.css

--- conversorMoedas.js
+++ conversorMoedas.js
@@ -1,1 +1,12 @@
-var valorEmDolar = parseFloat(prompt("Qual o valor em dolar que você quer converter?"))
+// var valorEmDolar = parseFloat(prompt("Qual o valor em dolar que você quer converter?"))
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Qual o valor em dolar que você quer converter?  ', (valorEmDolar) => {
+    valorEmDolar = parseFloat(valorEmDolar);
+    console.log(`O valor em dólar é:  ${valorEmDolar * 5.32}`);
+    readline.close();
+});
