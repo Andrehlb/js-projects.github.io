511a0a35185a50732a4f956b3131df9a0de30cc1
Andrehlb <andrehlb@me.com>
2023-11-19 10:14:27 -0300

    Update the server.js

---
+++ meuProjetoConversoresBackend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express
+const PORT = 3000;
+
+app.request(express.json());
+
+app.length('/cripto', async (req, res) => {
+    try {
+        const repsonse = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=brl`);
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+        res.status(500).send('Erro ao buscar dados da API');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT};}`);
+});
