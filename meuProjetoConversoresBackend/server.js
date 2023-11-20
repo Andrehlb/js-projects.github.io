a8dd5067b75708dc81c9f91256d98399ab45d459
Andrehlb <andrehlb@me.com>
2023-11-20 09:46:32 -0300

    Update of the server.js file to make a request to the Bitfinex API to get the criyptocurrency price.

--- meuProjetoConversoresBackend/server.js
+++ meuProjetoConversoresBackend/server.js
@@ -10,16 +10,27 @@ app.use(express.json());
 
 app.get('/cripto', async (req, res) => {
     try {
-        const cripto = req.query.cripto;
+        const cripto = req.query.cripto; // Exemplo: 'btc-bitcoin'.
+        const valorReais = req.query..valor // Exemplo: 100.
+
+        // Busca a taxa de câmbio de USD para BRL
+        const taxaCambio = await axios.get('https://api.exchangerate-api.com/v4/latest/USD');
+        const taxaCambioUSDparaBRL = taxaCambio.data.rates.BRL;
+
+        // Busca o preço da criptomoeda em USD
         const baseUrl = "https://api-pub.bitfinex.com/v2/";
         const pathParams = "ticker";
         const queryParams = `t${cripto}USD`;
+        const responseCripto = await axios.get(`${baseUrl}/${pathParams}/${queryParams}`);
+        const dataCripto = responseCripto.data[6];
 
-        const response = await axios.get(`${baseUrl}/${pathParams}/${queryParams}`);
-        const data = response.data;
-        res.send({ brl: data[6]}); // Envia o preço da criptomoeda em BRL (Reais).
+        // Calcula o valor da criptomoeda em BRL (Reais)
+        const valorCripto = (valorReais / (precoCriptoEmUSD * taxaCambioUSDparaBRL)).toFixed(8);
+
+        res.send({ valorConvertido: valorEmCripto });
     } catch (error) {
         console.error('Erro ao buscar dados da API:', error);
+        res.status(500).send('Erro ao buscar dados da API');
     }
 });
 
