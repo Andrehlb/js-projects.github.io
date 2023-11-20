833f43d7cb73c3632a732e5b6fb151450d0865dc
Andrehlb <andrehlb@me.com>
2023-11-20 15:19:05 -0300

    Update of the server.js file to make a request to the Bitfinex API to get the criyptocurrency price.

--- meuProjetoConversoresBackend/server.js
+++ meuProjetoConversoresBackend/server.js
@@ -17,30 +17,35 @@ async function getTaxaCambioUSDparaBRL() {
 
 // Função para obter o preço da criptomoeda em USD
 async function getPrecoCriptoEmUSD(cripto) {
-    const baseUrl = "https://api-pub.bitfinex.com/v2/";
-    const pathParams = "ticker";
+    const baseUrl = "https://api-pub.bitfinex.com/v2/ticker";
     const queryParams = `t${cripto}USD`;
-    const responseCripto = await axios.get(`${baseUrl}/${pathParams}/${queryParams}`);
-    const dataCripto = responseCripto.data[6];
+    const responseCripto = await axios.get(`${baseUrl}/${queryParams}`);
+    return responseCripto.data[6];
 }
 
 app.get('/cripto', async (req, res) => {
     try {
-        const cripto = req.query.cripto; // Exemplo: 'btc-bitcoin'.
-        const valorReais = req.query.valor // Exemplo: 100.
-        const taxaCambioUSDparaBRL = await getTaxaCambioUSDparaBRL();
-        const precoCriptoEmUSD = await getPrecoCriptoEmUSD(cripto);
+        const cripto = req.query.cripto; // Exemplo: 'BTC'
+        const simbolosValidos = ['BTC', 'ETH']; // Use apenas a parte relevante do símbolo
 
-        // Calcula o valor da criptomoeda em BRL (Reais)
-        const valorCripto = (valorReais / (precoCriptoEmUSD * taxaCambioUSDparaBRL)).toFixed(8);
+        // Validando o símbolo da criptomoeda
+        if (!simbolosValidos.includes(cripto)) {
+            res.status(400).send('Símbolo da criptomoeda inválido.');
+            return;
+        }
 
-        res.send({ valorConvertido: valorCripto });
+        // Obtendo o preço da criptomoeda
+        const precoCriptoUSD = await getPrecoCriptoEmUSD(cripto);
+
+        // Enviando a resposta ao cliente
+        res.json({ preco: precoCriptoUSD });
+
     } catch (error) {
-        console.error('Erro ao buscar dados da API:', error);
-        res.status(500).send('Erro ao buscar dados da API');
+        console.error('Erro ao processar requisição:', error);
+        res.status(500).send('Erro interno do servidor');
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT};}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
