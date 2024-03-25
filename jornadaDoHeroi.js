1db2e98e345b04848201108d9437024b66e9359a
Andrehlb <andrehlb@me.com>
2024-03-25 12:59:18 -0300

    feat(function): Add function to Calculate XP gains.

---
+++ jornadaDoHeroi.js
@@ -0,0 +1,29 @@
+//Desafios JavaScript com as funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+// Para converter os valores inseridos na entrada, usar a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
+
+// como devo fazer o código para que ele funcione corretamente?
+
+
+// Função para calcular XP ganhos em uma batalha
+function calculoXP(num1, num2) {
+    return num1 * num2 * 100;
+}
+
+Ler nivelMonstro (num1)
+Ler nivrlDificuldade (num2)
+
+Se num2 < 1 ou num2 > 100
+Entao
+Escrever "Dificuldade da batalha inválida"
+Terminar
+FimSE
+
+xpGanhos = calculoXP(nivelMonstro, nivelDificuldade)
+
+Escrever "Você ganhou ", xpGanhos, "XP!"
+Fim
+
+
